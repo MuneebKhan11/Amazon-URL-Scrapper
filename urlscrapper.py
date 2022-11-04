4187b4df31c97516b53bef92f6c1c9c3604c7eba
Muhammad Muneeb Khan <73933221+MuneebKhan11@users.noreply.github.com>
2022-11-04 18:34:17 +0000

    Add files via upload

---
+++ urlscrapper.py
@@ -0,0 +1,104 @@
+from bs4 import BeautifulSoup
+import requests
+from tkinter import *
+
+
+
+
+
+
+
+def get_title(soup):
+	
+	try:
+		
+		title = soup.find("span", attrs={"id":'productTitle'})
+
+		title_value = title.string
+
+		# Title as a string value
+		title_string = title_value.strip()
+
+		
+
+	except AttributeError:
+		title_string = ""	
+
+	return title_string
+
+
+def get_price(soup):
+
+	try:
+		price = soup.find("span", attrs={'id':'priceblock_ourprice'}).string.strip()
+
+	except AttributeError:
+		price = ""	
+
+	return price
+
+def get_rating(soup):
+
+	try:
+		rating = soup.find("i", attrs={'class':'a-icon a-icon-star a-star-4-5'}).string.strip()
+		
+	except AttributeError:
+		
+		try:
+			rating = soup.find("span", attrs={'class':'a-icon-alt'}).string.strip()
+		except:
+			rating = ""	
+
+	return rating
+
+
+def get_review_count(soup):
+	try:
+		review_count = soup.find("span", attrs={'id':'acrCustomerReviewText'}).string.strip()
+		
+	except AttributeError:
+		review_count = ""	
+
+	return review_count
+
+
+def get_availability(soup):
+	try:
+		available = soup.find("div", attrs={'id':'availability'})
+		available = available.find("span").string.strip()
+
+	except AttributeError:
+		available = ""	
+
+	return available
+
+
+def Exit():
+    root.destroy()	
+
+if __name__ == '__main__':
+
+	# Headers for request
+	HEADERS = ({'User-Agent':
+	            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+	            'Accept-Language': 'en-US, en;q=0.5'})
+
+	# The webpage URL
+	URL = input("Please Enter a URL of an amazon product: \n")
+
+	# HTTP Request
+	webpage = requests.get(URL, headers=HEADERS)
+
+	# Soup Object containing all data
+	soup = BeautifulSoup(webpage.content, "lxml")
+
+	# Function calls to display all necessary product information
+	print("Product Title =", get_title(soup))
+	print("Product Price =", get_price(soup))
+	print("Product Rating =", get_rating(soup))
+	print("Number of Product Reviews =", get_review_count(soup))
+	print("Availability =", get_availability(soup))
+	print()
+	print()
+
+
